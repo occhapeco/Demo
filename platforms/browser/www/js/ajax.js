af20b1a92f0ef8ae91b3559d285abd68f332f6ae
OC - Gabriel <occhapecosenai@gmail.com>
2016-12-12 18:18:48 -0200

    outro

--- platforms/browser/www/js/ajax.js
+++ platforms/browser/www/js/ajax.js
@@ -1,5 +1,5 @@
 var xhrTimeout=1000;
-var url='http://noxgames.com.br/service/';
+var url='http://olar.esy.es/';
 var urn = 'urn:service';
 
 var myApp = new Framework7({
@@ -33,6 +33,10 @@ $$(document).on('pageInit', function (e) {
       document.getElementById('atend1').checked = true;
     }
 
+    if(page.name === 'meus_cupons'){
+      carregar_meus_cupons();
+    }
+
 });
 
 var ptrContent = $$('.pull-to-refresh-content');
@@ -49,22 +53,33 @@ $$('.infinite-scroll').on('infinite', function () {
 
 });
 
+document.getElementById('cupons').innerHTML = '';
+carregar_cupons(0,1,0,0,"");
+
 function carregar_cupons(ultimo_carregado, cidade_id, delivery,pagamento,tipo_id){
   myApp.showPreloader();
   setTimeout(function () {
     json_dados = ajax_method(false,'usuario.select_cupons',localStorage.getItem("user_id"),cidade_id,delivery,pagamento,tipo_id,ultimo_carregado);
     var cupons = JSON.parse(json_dados);
 
-    for (i = 0;cupons.length ; i++) {
+    for (i = 0; i < cupons.length ; i++) {
 
       desconto = Math.round(100 - (cupons[i].preco_cupom * 100 / cupons[i].preco_normal));
 
+      if (cupons[i].preco_cupom % 1 == 0) {
+        cupons[i].preco_cupom  =  cupons[i].preco_cupom + ',00';
+      }
+
+      if (cupons[i].preco_normal % 1 == 0) {
+        cupons[i].preco_normal  =  cupons[i].preco_normal + ',00';
+      }
+
       document.getElementById('cupons').innerHTML += '<div class="card facebook-card">'+
                                                         '<div class="card-header no-border" style="padding-bottom: 1px;">'+
-                                                          '<div class="facebook-name" style="font-size: 18px; color: black; font-family: tahoma;font-weight: bold">Pizza grande + potatos</div>'+
+                                                          '<div class="facebook-name" style="font-size: 18px; color: black; font-family: tahoma;font-weight: bold">'+cupons[i].titulo+'</div>'+
                                                        ' </div>'+
                                                            '<div class="facebook-name" style="margin-left: 15px;">'+cupons[i].nome_fantasia+'</div>'+
-                                                        '<div class="card-content"><a href="oferta.html"><!-- colocar aqui os valores pra testar dps --><img src="img/pizza.jpg" width="100%">'+
+                                                        '<div class="card-content"><a href="#" onclick="detalhes_cupom('+cupons[i].id+',`'+cupons[i].titulo+'`,'+desconto+',`'+cupons[i].preco_normal+'`,`'+cupons[i].preco_cupom+'`,`'+cupons[i].prazo+'`,'+cupons[i].quantidade+',`'+cupons[i].nome_fantasia+'`,`http://www.olar.esy.es/'+cupons[i].caminho+'`);"><img src="http://www.olar.esy.es/'+cupons[i].caminho+'" width="100%">'+
                                                           '<div style="position:absolute; right: 20px; z-index: 999999;top: 10px;color: white; background-color: rgba(255, 0, 0,0.85); padding: 10px; border-radius: 5px; font-weight: bold; ">'+
                                                             '<i class="fa fa-ticket"></i> '+cupons[i].quantidade+
                                                             '</div>'+
@@ -72,7 +87,7 @@ function carregar_cupons(ultimo_carregado, cidade_id, delivery,pagamento,tipo_id
                                                       '  </div>'+
                                                         '<div class="card-footer no-border">'+
                                                          ' <p style="font-size: 15px;color: coral;">'+desconto+'% off</p>'+
-                                                          '<center><diva style="font-size: 20px;color: #007aff;">R$'+cupons[i].preco_cupom +'</diva><br><s>R$'+cupons[i].preco_normal+'</s></center>'+
+                                                          '<center><diva style="font-size: 20px;color: #007aff;">R$'+cupons[i].preco_cupom+'</diva><br><s>R$'+cupons[i].preco_normal+'</s></center>'+
                                                           '<a href="oferta.html?id='+cupons[i].id+'" class="link" style="font-size: 23px;"><i class="fa fa-cart-arrow-down"></i></a>'+
                                                         '</div>'+
                                                      ' </div>';
@@ -88,32 +103,34 @@ function carregar_meus_cupons(){
     json_dados = ajax_method(false,'usuario.select_historico',localStorage.getItem("user_id"));
     var cupons = JSON.parse(json_dados);
     document.getElementById('meus_cupons_historico').innerHTML = ' ';
+    var batatinea = ' ';
     var last = ' ';
+    alert(json_dados);
 
-    for (i = 0;cupons.length ; i++) {
+    for (i = 0;i < cupons.length ; i++) {
 
       if (cupons[i].data_resgate != last) {
           last = cupons[i].data_resgate;
-          document.getElementById('meus_cupons_historico').innerHTML +=  '<div class="content-block-title" ><i class="fa fa-calendar"></i>'+cupons[i].data_resgate+'</div>';
+          batatinea +=  '<div class="content-block-title" ><i class="fa fa-calendar"></i>'+cupons[i].data_resgate+'</div>';
       }
 
       desconto = Math.round(100 - (cupons[i].preco_cupom * 100 / cupons[i].preco_normal));
 
-      document.getElementById('meus_cupons_historico').innerHTML +=   '<div class="card">'+
+      batatinea +=   '<div class="card">'+
                                                                        '<div class="card-content">'+
                                                                          '<div class="list-block media-list">'+
                                                                             '<ul>'+
                                                                               '<li>';
       if (cupons[i].estado == 0) 
-        document.getElementById('meus_cupons_historico').innerHTML += '<a href="#" class="item-content item-link" style="border-left: thick solid #007aff;">';
+        batatinea += '<a href="#" class="item-content item-link" style="border-left: thick solid #007aff;">';
       if (cupons[i].estado == 1) 
-        document.getElementById('meus_cupons_historico').innerHTML += '<a href="#" class="item-content item-link" style="border-left: thick solid #FFa500;">';
+        batatinea += '<a href="#" class="item-content item-link" style="border-left: thick solid #FFa500;">';
       if (cupons[i].estado == 2)
-        document.getElementById('meus_cupons_historico').innerHTML += '<a href="#" class="item-content item-link" style="border-left: thick solid #FF0000;">'; 
+        batatinea += '<a href="#" class="item-content item-link" style="border-left: thick solid #FF0000;">'; 
       if (cupons[i].estado == 3) 
-        document.getElementById('meus_cupons_historico').innerHTML += '<a href="#" class="item-content item-link" style="border-left: thick solid #00FF00;">';
+        batatinea += '<a href="#" class="item-content item-link" style="border-left: thick solid #00FF00;">';
 
-      document.getElementById('meus_cupons_historico').innerHTML +=   '<div class="item-inner">'+
+      batatinea +=                                                     '<div class="item-inner">'+
                                                                         '<div class="item-title-row">'+
                                                                           '<div class="item-title">'+cupons[i].titulo+'</div>'+
                                                                         '</div>'+
@@ -127,38 +144,57 @@ function carregar_meus_cupons(){
                                                             '<div class="card-footer">'+
                                                               '<span>'+desconto+'% Off</span>';
       if (cupons[i].estado == 0)
-        document.getElementById('meus_cupons_historico').innerHTML += '<span>Cupom aguardando resgate</span>';
+        batatinea += '<span>Cupom aguardando resgate</span>';
       if (cupons[i].estado == 1)
-        document.getElementById('meus_cupons_historico').innerHTML += '<span>Avaliação pendente</span>';
+        batatinea += '<span>Avaliação pendente</span>';
       if (cupons[i].estado == 2)
-        document.getElementById('meus_cupons_historico').innerHTML += '<span>Cupom Não utilizado</span>';
+        batatinea += '<span>Cupom Não utilizado</span>';
       if (cupons[i].estado == 3)
-        document.getElementById('meus_cupons_historico').innerHTML += '<span>Cupom Finalizado</span>';
+        batatinea += '<span>Cupom Finalizado</span>';
 
-      document.getElementById('meus_cupons_historico').innerHTML +=     '</div>'+
+      batatinea +=                                                       '</div>'+
                                                                        '</div>';
     }   
+    document.getElementById('meus_cupons_historico').innerHTML = batatinea;
   myApp.hidePreloader();
 },100);
 }
 
-function detalhes_cupom(id,nome,desconto,preco_ini,preco_desc,prazo,quantidade,empresa){
+function detalhes_cupom(id,nome,desconto,preco_ini,preco_desc,prazo,quantidade,empresa,imagem){
   myApp.showPreloader();
+  mainView.router.loadPage('oferta.html');
   setTimeout(function () {
     json_dados = ajax_method(false,'usuario.select_detalhes_cupom',id);
     if(json_dados){
       var cupom = JSON.parse(json_dados);
       set_inner("empresa_oferta",empresa);
-     // document.getElementById('img_oferta').setAttribute("src", cupom[0].caminho); precisamos do caminho do site
-     set_inner("desconto_cupom",desconto);
-     set_inner("nome_cupom",nome);
-     set_inner("precos_cupom",'Por: R$'+preco_desc+' &nbsp<s style="color:gray">De: R$'+preco_ini+'</s>');
-     set_inner("info_cupom",'<i class="fa fa-ticket"></i> '+quantidade+' Restantes<br><i class="fa fa-calendar"></i> Válido até'+prazo);
-     set_inner("desc_cupom",'<p>'+cupom[0].descricao+'</p>');
-     set_inner("regras_cupom",'<p>'+cupom[0].regras+'</p>');
-     set_inner("empresa_cupom",empresa); 
-     document.getElementById('telefone_cupom').setAttribute("onclick", "window.open('tel:"+cupom[0].telefone+"', '_system');");
-     document.getElementById("endereco_cupom").setAttribute("onclick", "window.open('http://maps.apple.com/?ll="+cupom[0].latitude+","+cupom[0].longitude+"', '_system');");
+       document.getElementById('img_oferta').setAttribute("src", imagem);
+       set_inner("desconto_cupom",desconto+'% off');
+       set_inner("nome_cupom",nome);
+       set_inner("precos_cupom",'Por: R$'+preco_desc+' &nbsp<s style="color:gray">De: R$'+preco_ini+'</s>');
+       set_inner("info_cupom",'<i class="fa fa-ticket"></i> '+quantidade+' Restantes<br><i class="fa fa-calendar"></i> Válido até'+prazo);
+       set_inner("desc_cupom",'<p>'+cupom.detalhes.descricao+'</p>');
+       set_inner("regras_cupom",'<p>'+cupom.detalhes.regras+'</p>');
+       set_inner("empresa_cupom",empresa); 
+       document.getElementById('telefone_cupom').setAttribute("onclick", "window.open('tel:"+cupom.detalhes.telefone+"', '_system');");
+       document.getElementById("endereco_cupom").setAttribute("onclick", "window.open('http://maps.apple.com/?q=loc:"+cupom.detalhes.latitude+","+cupom.detalhes.longitude+"', '_system');");
+
+       json_dados = ajax_method(false,'usuario.select_historico',localStorage.getItem("user_id"));
+       var permite = JSON.parse(json_dados);
+
+       var ok = true;
+       for (i = 0; i < permite.length; i++) {
+         if (permite[i].cupom_id == id)
+            ok = false;
+       }
+
+     if (ok) {
+      document.getElementById('botao_cupom').setAttribute('onclick',"pegar_cupom("+id+");");
+     }else{
+      document.getElementById('botao_cupom').innerHTML = '<i class="fa fa-cart-arrow-down" style="font-size: 20px;"></i> &nbsp&nbspCupom Adquirido';
+      document.getElementById('botao_cupom').setAttribute('disabled'," ");
+      document.getElementById('botao_cupom').setAttribute('class',"button button-fill color-orange bttnb");
+     }
 
     }else{
       myApp.alert("Não foi possível carregar os detalhes do cupom. Tente novamente.");
@@ -274,6 +310,16 @@ function set_value(id,valor){
 
 function set_inner(id,valor){
    document.getElementById(id).innerHTML = valor;
+}
+
+function pegar_cupom(id){
+    json_dados = ajax_method(false,'usuario.pegar_cupom',localStorage.getItem("user_id"),id);
+    if (json_dados) {
+      myApp.alert("Cupom ativado, boas compras!");
+      mainView.router.back();
+    }else{
+      myApp.alert("Não foi possível pegar este cupom, tente novamente.");
+    }
 }
 
 function ajax_method()
